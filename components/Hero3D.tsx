0daf76b6abd87dbd6f5d3bd46250c40451556d96
S Chiranjeevi <schiru330@gmail.com>
2026-09-14 01:12:08 +0530

    Add 3D hero iframe

---
+++ components/Hero3D.tsx
@@ -0,0 +1,1 @@
+ export default function Hero3D() { return ( <iframe src=""https://my.spline.design/genkubgreetingrobot-661g8sd9rDfzqWqMWUnC2Zc7/" style={{ width: '100%', height: '100%', border: 'none' }} title="3D scene" /> ); }
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,19 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  webpack: (config) => {
+    config.experiments = {
+      ...config.experiments,
+      asyncWebAssembly: true,
+      layers: true,
+    };
+    config.module.rules.push({
+      test: /\.wasm$/,
+      type: "asset/resource",
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
